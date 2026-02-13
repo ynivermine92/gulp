fb2741c894226fcdaf13441cbaf48de37733e0cd
ynivermine92 <ynivermine92@gmail.com>
2026-02-13 16:47:08 +0200

    Acardion left  v2

--- src/components/Accordion/Acartion left/acardion.js
+++ src/components/Accordion/Acartion left/acardion.js
@@ -1,26 +1,37 @@
   const accordion = () => {
     const accordinBox = document.querySelectorAll('.acardion__item-box');
-    const accordinContent = document.querySelectorAll('.acardion__item-content');
 
     accordinBox.forEach((item, index) => {
+      const mobileContent = item.querySelector('.acardion__mobile');
+      const desktopContentList = document.querySelectorAll('.acardion__item-content');
+
+      // Изначально скрываем мобильный контент, кроме активного
+      if (!item.classList.contains('active')) {
+        if (mobileContent) mobileContent.style.maxHeight = '0px';
+      } else {
+        if (mobileContent) mobileContent.style.maxHeight = mobileContent.scrollHeight + 'px';
+      }
+
       item.addEventListener('click', () => {
+        // Закрываем все мобильные блоки
+        accordinBox.forEach((box, i) => {
+          box.classList.remove('active');
+          const mobile = box.querySelector('.acardion__mobile');
+          if (mobile) mobile.style.maxHeight = '0px';
 
+          // Закрываем все десктопные блоки
+          if (desktopContentList[i]) desktopContentList[i].classList.remove('active');
+        });
 
-        accordinBox.forEach((box) => {
-          box.classList.remove('active')
-        })
-
-
-        accordinContent.forEach((content) => {
-          content.classList.remove('active')
-        })
-
-
+        // Открываем выбранный
         item.classList.add('active');
-        accordinContent[index].classList.add('active');
+        if (mobileContent) mobileContent.style.maxHeight = mobileContent.scrollHeight + 'px';
 
+        // Десктоп: добавляем active к соответствующему элементу
+        if (desktopContentList[index]) desktopContentList[index].classList.add('active');
       });
     });
   };
 
   accordion();
+
